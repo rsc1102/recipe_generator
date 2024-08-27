5f97f031b3470f02c83761e4051bc8915a8adead
rsc1102 <rohitchaudhari1102@gmail.com>
2024-08-27 18:48:56 -0400

    updated allowed_hosts

--- recipe_generator/settings.py
+++ recipe_generator/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False
 
-if DEBUG:
-    ALLOWED_HOSTS = ['.localhost','127.0.0.1','[::1]']
-else:
-    ALLOWED_HOSTS = ['.vercel.app']
+# if DEBUG:
+#     ALLOWED_HOSTS = ['.localhost','127.0.0.1','[::1]']
+# else:
+#     ALLOWED_HOSTS = ['.vercel.app']
+
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
