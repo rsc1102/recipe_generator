df2f9610a0a3b00e2ba57f7a1671c1853d344b20
rsc1102 <rohitchaudhari1102@gmail.com>
2024-08-28 14:10:55 -0400

    updated file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,22 @@
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --no-input --clear
+#!bin/bash
+
+#Create a virtual env
+echo "creating virtual env..."
+python -m venv venv
+source venv/bin/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+
