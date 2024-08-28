f0405c5493222d7da21d941f3ceecae433abf5bb
rsc1102 <rohitchaudhari1102@gmail.com>
2024-08-28 13:57:40 -0400

    updated file

--- recipe_generator/settings.py
+++ recipe_generator/settings.py
@@ -103,15 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
-
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
