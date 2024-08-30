5088b3b37fb9e34eac10961b0a2b97a30a9bdc8f
rsc1102 <rohitchaudhari1102@gmail.com>
2024-08-30 00:48:36 -0400

    updated file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Creating virtual env"
+python3.12 -m venv myenv
+source myenv/bin/activate
+
+echo "Upgrading pip"
+python3.12 -m pip install --upgrade pip
+
+echo "Installing dependencies"
 python3.12 -m pip install -r requirements.txt
+
+echo "Collecting static files"
 python3.12 manage.py collectstatic --no-input --clear
+
+echo "build_files.sh process complete. Folder list:"
+ls
--- recipe_generator/settings.py
+++ recipe_generator/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG') == "true"
 
-ALLOWED_HOSTS = ['.localhost','127.0.0.1','[::1]','.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,6 +102,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
